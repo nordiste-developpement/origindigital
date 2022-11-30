298eef514eaaeeee87171cc16cb5c90b09c4a957
Matthieu <Matthieu.Maginot@gmail.com>
2022-11-30 10:10:53 +0100

    hello

--- frontend/src/components/AuthentificatedRoute/PrivateRoute.jsx
+++ frontend/src/components/AuthentificatedRoute/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRoute = () => {
   const auth = { token: false };
-  return auth.token ? <Outlet /> : <Navigate to="/" />;
+  return auth.token ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
---
+++ frontend/src/pages/PageLogin.jsx
@@ -0,0 +1,5 @@
+const PageLogin = () => {
+  return <h1>LOGIN</h1>;
+};
+
+export default PageLogin;
